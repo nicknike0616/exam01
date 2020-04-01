acadd740768de03b23cb9fac75f5eea81cbd62b1
nicknike0616 <nicknike0616@gmail.com>
2020-04-01 17:51:15 +0800

    exam01

--- uled/main.cpp
+++ uled/main.cpp
@@ -1,23 +1,29 @@
-/*Draw a square and display your student ID on uLCD.
+
 #include "mbed.h"
 
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
+PwmOut PWM1(D6);
+
+AnalogIn Ain(A0);
+
 int main()
 {
-    // basic printf demo = 16 by 18 characters on screen
-
-    uLCD.printf("\n106061255\n"); //Default Green on black text
+    uLCD.printf("\n106061255\n");
 
     uLCD.line(30, 30, 40, 30, WHITE);
     uLCD.line(40, 30, 40, 40, WHITE);
     uLCD.line(40, 40, 30, 40, WHITE);
     uLCD.line(30, 40, 30, 30, WHITE);
 
+    float i;
+    while(1){
+        for( i=0; i<1; i+=0.1 ){
+        PWM1.period(0.001);
+        PWM1 = Ain;
+        wait(0.1);
+        }
+    }
 }
-/*
-
-
-
